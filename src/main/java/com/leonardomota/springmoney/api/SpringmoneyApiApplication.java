5eb67062f31950576918db8d094a8761b78b3c8f
Leonardo Mota <leonardomota14@gmail.com>
2019-06-07 15:38:18 -0300

    Primeiro commit com cricao do projeto e entity categoria, com bd mysql

---
+++ src/main/java/com/leonardomota/springmoney/api/SpringmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.leonardomota.springmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/leonardomota/springmoney/api/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.leonardomota.springmoney.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leonardomota.springmoney.api.model.Categoria;
+import com.leonardomota.springmoney.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> lista() {
+		return categoriaRepository.findAll();
+	}
+}
