38b46faef7b219270596d613efc81773a7392cb1
Leonardo Mota <leonardomota14@gmail.com>
2019-06-09 01:59:55 -0300

    Usando eventos para adicionar header Location

--- src/main/java/com/leonardomota/springmoney/api/resource/CategoriaResource.java
+++ src/main/java/com/leonardomota/springmoney/api/resource/CategoriaResource.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.leonardomota.springmoney.api.event.RecursoCriadoEvent;
 import com.leonardomota.springmoney.api.model.Categoria;
 import com.leonardomota.springmoney.api.repository.CategoriaRepository;
 
@@ -26,6 +29,9 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Categoria> lista() {
 		return categoriaRepository.findAll();
@@ -34,12 +40,8 @@ public class CategoriaResource {
 	@PostMapping
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria catSalva = categoriaRepository.save(categoria);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{codigo}")
-				.buildAndExpand(categoria.getCodigo()).toUri();
-		//response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(catSalva);		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, catSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(catSalva);
 	}
 	
 	@GetMapping("/{codigo}")
--- src/main/java/com/leonardomota/springmoney/api/resource/PessoaResource.java
+++ src/main/java/com/leonardomota/springmoney/api/resource/PessoaResource.java
@@ -1,10 +1,11 @@
 package com.leonardomota.springmoney.api.resource;
 
-import java.net.URI;
-
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.leonardomota.springmoney.api.event.RecursoCriadoEvent;
 import com.leonardomota.springmoney.api.model.Pessoa;
 import com.leonardomota.springmoney.api.repository.PessoaRepository;
 
@@ -24,14 +25,14 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;	
+	
 	@PostMapping
-	public ResponseEntity<Pessoa> criar (@Valid @RequestBody Pessoa pessoa) {
+	public ResponseEntity<Pessoa> criar (@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{/codigo}")
-				.buildAndExpand(pessoa.getcodigo()).toUri();
-		
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getcodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
